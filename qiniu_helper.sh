28337ae0549bcab6475b78a982c6f4549409a859
dongjiaqiang <geekdongjq@gmail.com>
2015-08-21 20:58:00 +0800

    update

--- qiniu_helper.sh
+++ qiniu_helper.sh
@@ -1,18 +1,64 @@
 #!/bin/bash
 
-readonly ACCESS_KEY=gCS_AUyK7LgfEa9aYEn-O2acLXrL8cGpashfOdBQ
-
+readonly ACddCESS_KEY=gCS_AUyK7LgfEa9aYEn-O2acLXrL8cGpashfOdBQ
 readonly SECRET_KEY=knVH7CIYMV7T9TJZWEsYsTHFNoe3Sn15b1QQaFOq
 
+version=1.0.0
 
 
+readonly http_head_content_type='Content-Type:application/x-www-form-urlencoded'
+readonly auth_common='Authorization:QBox'
+readonly CONFIG_FILE_LOCATION="./qiniu_helper.config"
 
+#====================================url==========================================
 
+#删除资源url
+readonly API_DELETE_RESOURCE__URL="http://rs.qiniu.com/delete/"
 
+#移动资源url
+readonly API_MOVE_RESOURCE_URL="http://rs.qiniu.com/move/"
+
+#复制资源url
+readonly API_COPY_RESOURCE_URL="http://rs.qiniu.com/copy/"
+
+#获取资源信息url
+readonly API_STAT_RESOURCE_URL="http://rs.qiniu.com/stat/"
+
+#抓取资源信息url
+readonly API_FETCH_RESOURCE_URL="http://iovip.qbox.me/fetch/"
 
 
 
 #===================================common functions===================================
+
+#保存或更改基本配置信息
+save_config(){
+	if [  ! -e "$CONFIG_FILE_LOCATION" ]
+	then
+		touch $CONFIG_FILE_LOCATION
+		echo "ACCESS_KEY=$1" > $CONFIG_FILE_LOCATION
+		echo "SECRET_KEY=$2" >> $CONFIG_FILE_LOCATION
+		if [ ! -z "$3" ]
+		then
+			echo "OPERATION_LOG_FILE=$3" >> $CONFIG_FILE_LOCATION
+		fi
+       else
+		tmp=$(cat $CONFIG_FILE_LOCATION | sed -e 's\^ACCESS_KEY=.*$\ACCESS_KEY='$1'\'  -e 's\^SECRET_KEY=.*$\SECRET_KEY='$2'\')
+		echo "$tmp" > $CONFIG_FILE_LOCATION
+		if [ ! -z "$3" ]
+		then
+ 			if cat $CONFIG_FILE_LOCATION | grep '^OPERATION_LOG_FILE=.*$' > /dev/null
+			then
+		     		tmp=$(cat $CONFIG_FILE_LOCATION | sed -e 's\^OPERATION_LOG_FILE=.*$\OPERATION_LOG_FILE='$3'\')
+                     		echo "$tmp" > $CONFIG_FILE_LOCATION
+			else
+		     		echo "OPERATION_LOG_FILE=$3" >> $CONFIG_FILE_LOCATION
+			fi
+		fi
+	fi
+}
+
+
 
 get_unixtime(){
 	if [ "$#" -eq 0 ]
@@ -71,25 +117,24 @@ get_json_value(){
 }
 
 delete_resource(){
-	bucket=$1
-	key=$2
-	safe_base64_url=$(safe_base64_encode_url "$bucket:$key")
+	safe_base64_url=$(safe_base64_encode_url "$1:$2")
 	access_token=$(generate_access_token "/delete/$safe_base64_url\n")
-	curl -s -H 'Content-Type: application/x-www-form-urlencoded' -H "Authorization:QBox $access_token" -X POST "http://rs.qiniu.com/delete/$safe_base64_url"
+	curl -s -H $http_head_content_type -H "$auth_common $access_token" -X POST "${API_DELETE_RESOURCE__URL}${safe_base64_url}"
 }
 
 move_resource(){
-	src_bucket=$1
-	src_key=$2
-	dest_bucket=$3
-	dest_key=$4
-	src_safe_base64_url=$(safe_base64_encode_url "$src_bucket:$src_key")
-	dest_safe_base64_url=$(safe_base64_encode_url "$dest_bucket:$dest_key")
+	src_safe_base64_url=$(safe_base64_encode_url "$1:$2")
+	dest_safe_base64_url=$(safe_base64_encode_url "$3:$4")
 	access_token=$(generate_access_token "/move/$src_safe_base64_url/$dest_safe_base64_url\n")
-	curl -s -H 'Content-Type: application/x-www-form-urlencoded' -H "Authorization:QBox $access_token" -X POST "http://rs.qiniu.com/move/$src_safe_base64_url/$dest_safe_base64_url"
-
+	curl -s -H $http_head_content_type -H "$auth_common $access_token" -X POST "${API_MOVE_RESOURCE_URL}${src_safe_base64_url}/${dest_safe_base64_url}"
 }
 
+copy_resource(){
+	src_safe_base64_url=$(safe_base64_encode_url "$1:$2")
+        dest_safe_base64_url=$(safe_base64_encode_url "$3:$4")
+	access_token=$(generate_access_token "/copy/$src_safe_base64_url/$dest_safe_base64_url\n")
+	curl -s -H $http_head_content_type -H "$auth_common $access_token" -X POST "${API_COPY_RESOURCE_URL}${src_safe_base64_url}/${dest_safe_base64_url}"
+}
 
 
 get_resource_info(){
@@ -119,13 +164,7 @@ exit 1
 }
 #==================main()===========================
 
+move_resource "mytest" "r.txt" "mytest" "s.txt"
+#copy_resource "mytest" "r.txt" "mytest" "b.txt"
 
-get_resource_info "mytest" "config.txt"
-echo
-get_resource_info "mytest" "1.mp3"
-echo
-get_resource_info "mytest" 
-echo
-delete_resource "mytest" "config.txt"
-
-move_resource "mytest" "readme.txt" "mytest" "r.txt"
+save_config 1 23 dsdff 
